cde81beac738c8f95bdcb1adf637029e713b9df5
Arsh <arsh291206@gmail.com>
2020-04-03 18:02:58 +0530

    data modified

--- src/com/algorithms/naivebayes/datasets/Data.java
+++ src/com/algorithms/naivebayes/datasets/Data.java
@@ -25,18 +25,20 @@ public class Data {
                         { "John", "brown", "average", "heavy", "no", "none" },
                         { "Katie", "brown", "short", "light", "yes", "none" }, };
 
-        static String[][] CAR = { { "red", "sports", "domestic", "yes" }, { "red", "sports", "domestic", "no" },
-                        { "red", "sports", "domestic", "yes" }, { "yellow", "sports", "domestic", "no" },
-                        { "yellow", "sports", "imported", "yes" }, { "yellow", "suv", "imported", "no" },
-                        { "yellow", "suv", "imported", "yes" }, { "yellow", "suv", "domestic", "no" },
-                        { "red", "suv", "imported", "no" }, { "red", "sports", "imported", "yes" }, };
+        static String[][] CAR = { { "color", "type", "origin", "stolen" }, { "red", "sports", "domestic", "yes" },
+                        { "red", "sports", "domestic", "no" }, { "red", "sports", "domestic", "yes" },
+                        { "yellow", "sports", "domestic", "no" }, { "yellow", "sports", "imported", "yes" },
+                        { "yellow", "suv", "imported", "no" }, { "yellow", "suv", "imported", "yes" },
+                        { "yellow", "suv", "domestic", "no" }, { "red", "suv", "imported", "no" },
+                        { "red", "sports", "imported", "yes" }, };
 
-        static String[][] INCOME = { { "<=30", "high", "no", "fair", "no" },
-                        { "<=30", "high", "no", "excellent", "no" }, { "31...40", "high", "no", "fair", "yes" },
-                        { ">40", "medium", "no", "fair", "yes" }, { ">40", "low", "yes", "fair", "yes" },
-                        { ">40", "low", "yes", "excellent", "no" }, { "31...40", "low", "yes", "excellent", "yes" },
-                        { "<=30", "medium", "no", "fair", "no" }, { "<=30", "low", "yes", "fair", "yes" },
-                        { ">40", "medium", "yes", "fair", "yes" }, { "<=30", "medium", "yes", "excellent", "yes" },
+        static String[][] INCOME = { { "age", "income", "student", "credit_rating", "buys_computer" },
+                        { "<=30", "high", "no", "fair", "no" }, { "<=30", "high", "no", "excellent", "no" },
+                        { "31...40", "high", "no", "fair", "yes" }, { ">40", "medium", "no", "fair", "yes" },
+                        { ">40", "low", "yes", "fair", "yes" }, { ">40", "low", "yes", "excellent", "no" },
+                        { "31...40", "low", "yes", "excellent", "yes" }, { "<=30", "medium", "no", "fair", "no" },
+                        { "<=30", "low", "yes", "fair", "yes" }, { ">40", "medium", "yes", "fair", "yes" },
+                        { "<=30", "medium", "yes", "excellent", "yes" },
                         { "31...40", "medium", "no", "excellent", "yes" }, { "31...40", "high", "yes", "fair", "yes" },
                         { ">40", "medium", "no", "excellent", "no" }, };
 
