6e87334ee28ba0286169f69601e7e62ad4968dd1
Adnan Hakim <adnanhakim007@gmail.com>
2020-04-03 17:38:50 +0530

    added map

--- src/com/algorithms/naivebayes/datasets/Data.java
+++ src/com/algorithms/naivebayes/datasets/Data.java
@@ -1,5 +1,9 @@
 package com.algorithms.naivebayes.datasets;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Data {
 
     static String[][] WEATHER = { { "outlook", "temperature", "humidity", "windy", "class" },
@@ -19,4 +23,12 @@ public class Data {
             { "Emily", "red", "average", "heavy", "no", "sunburned" },
             { "Pete", "brown", "tall", "heavy", "no", "none" }, { "John", "brown", "average", "heavy", "no", "none" },
             { "Katie", "brown", "short", "light", "yes", "none" }, };
+
+    static Map<String, String[][]> datas = Collections.unmodifiableMap(new HashMap<String, String[][]>() {
+        private static final long serialVersionUID = 1L;
+        {
+            put("WEATHER", WEATHER);
+            put("TAN", TAN);
+        }
+    });
 }
