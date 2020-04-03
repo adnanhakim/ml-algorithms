8b75dc722e2f7a8b0743cc9b332e66786e3a11b1
Adnan Hakim <adnanhakim007@gmail.com>
2020-04-03 17:33:14 +0530

    dataset 3 added

--- src/com/algorithms/naivebayes/datasets/Data.java
+++ src/com/algorithms/naivebayes/datasets/Data.java
@@ -9,5 +9,14 @@ public class Data {
             { "overcast", "cool", "normal", "true", "P" }, { "sunny", "mild", "high", "false", "N" },
             { "sunny", "cool", "normal", "false", "P" }, { "rain", "mild", "normal", "false", "P" },
             { "sunny", "mild", "normal", "true", "P" }, { "overcast", "mild", "high", "true", "P" },
-            { "overcast", "hot", "normal", "false", "P" }, { "rain", "mild", "high", "true", "N" }, };
+            { "overcast", "hot", "normal", "false", "P" }, { "rain", "mild", "high", "true", "N" } };
+
+    static String[][] TAN = { { "name", "hair", "height", "weight", "dublin", "result" },
+            { "Sarah", "blonde", "average", "light", "no", "sunburned" },
+            { "Dana", "blonde", "tall", "average", "yes", "none" },
+            { "Alex", "brown", "short", "average", "yes", "none" },
+            { "Annie", "blonde", "short", "average", "no", "sunburned" },
+            { "Emily", "red", "average", "heavy", "no", "sunburned" },
+            { "Pete", "brown", "tall", "heavy", "no", "none" }, { "John", "brown", "average", "heavy", "no", "none" },
+            { "Katie", "brown", "short", "light", "yes", "none" }, };
 }
