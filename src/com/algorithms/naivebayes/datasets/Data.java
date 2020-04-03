49bc68b877effe9754e8534a52430aa6ba228e00
Arsh <arsh291206@gmail.com>
2020-04-03 17:43:52 +0530

    data completed

--- src/com/algorithms/naivebayes/datasets/Data.java
+++ src/com/algorithms/naivebayes/datasets/Data.java
@@ -28,11 +28,11 @@ public class Data {
                         { "red", "suv", "imported", "no" }, { "red", "sports", "imported", "yes" }, };
 
         static String[][] INCOME = { { "<=30", "high", "no", "fair", "no" },
-                        { "<=30", "high", "no", "excellent", "no" }, { "31...40", "high", "no", "fair", "no" },
-                        { ">40", "medium", "no", "fair", "no" }, { ">40", "low", "yes", "fair", "no" },
-                        { ">40", "low", "yes", "excellent", "no" }, { "31...40", "low", "yes", "excellent", "no" },
-                        { "<=30", "medium", "no", "fair", "no" }, { "<=30", "low", "yes", "fair", "no" },
-                        { ">40", "medium", "yes", "fair", "no" }, { "<=30", "medium", "yes", "excellent", "no" },
-                        { "31...40", "medium", "no", "excellent", "no" }, { "31...40", "high", "yes", "fair", "no" },
+                        { "<=30", "high", "no", "excellent", "no" }, { "31...40", "high", "no", "fair", "yes" },
+                        { ">40", "medium", "no", "fair", "yes" }, { ">40", "low", "yes", "fair", "yes" },
+                        { ">40", "low", "yes", "excellent", "no" }, { "31...40", "low", "yes", "excellent", "yes" },
+                        { "<=30", "medium", "no", "fair", "no" }, { "<=30", "low", "yes", "fair", "yes" },
+                        { ">40", "medium", "yes", "fair", "yes" }, { "<=30", "medium", "yes", "excellent", "yes" },
+                        { "31...40", "medium", "no", "excellent", "yes" }, { "31...40", "high", "yes", "fair", "yes" },
                         { ">40", "medium", "no", "excellent", "no" }, };
 }
